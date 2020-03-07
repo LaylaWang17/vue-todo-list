47cf8ae87b635dbb818e96623ae887931edba08c
Wang Lei <lei.wang1@thoughtworks.com>
2020-03-07 17:54:49 +0800

    chore: install element ui

---
+++ src/components/element-ui.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import { Input, Button, Link, Row, Col, Menu, MenuItem } from 'element-ui';
+
+Vue.use(Input);
+Vue.use(Button);
+Vue.use(Link);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Menu);
+Vue.use(MenuItem);
