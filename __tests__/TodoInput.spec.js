be2f3b83cae41abec6e032d71a69ae4f343f14fd
Wang Lei <lei.wang1@thoughtworks.com>
2020-03-13 18:04:45 +0800

    test: add tests for component TodoInput

---
+++ __tests__/TodoInput.spec.js
@@ -0,0 +1,26 @@
+import { mount } from "@vue/test-utils";
+import sinon from "sinon";
+
+import TodoInput from "../src/components/TodoInput.vue";
+import "../src/element-ui";
+
+describe("TodoInput", () => {
+  const defaultwrapper = mount(TodoInput);
+
+  test("is a Vue instance", () => {
+    expect(defaultwrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test("should placeholder is 'What needs to be done?'", () => {
+    expect(defaultwrapper.find("input").attributes("placeholder")).toBe(
+      "What needs to be done?"
+    );
+  });
+
+  test("should 'addTodo' method be called", () => {
+    const addTodo = sinon.stub();
+    defaultwrapper.setMethods({ addTodo: addTodo });
+    defaultwrapper.trigger("submit");
+    expect(addTodo.called).toBeTruthy();
+  });
+});
