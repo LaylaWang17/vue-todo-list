5f2b04cad497266f05a18faa86e33bdfd02132ab
Wang Lei <lei.wang1@thoughtworks.com>
2020-03-13 19:57:36 +0800

    test: add tests for component Todo

---
+++ __tests__/Todo.spec.js
@@ -0,0 +1,50 @@
+import { mount } from "@vue/test-utils";
+import Todo from "../src/components/Todo.vue";
+import "../src/element-ui";
+
+describe("Todo", () => {
+  const todoData = {
+    id: 1,
+    content: "test",
+    active: true
+  };
+
+  const activeTodoWrapper = mount(Todo, {
+    propsData: {
+      initialTodo: todoData
+    }
+  });
+
+  const completedTodoWrapper = mount(Todo, {
+    propsData: {
+      initialTodo: {
+        ...todoData,
+        active: false
+      }
+    }
+  });
+
+  test("is a Vue instance", () => {
+    expect(activeTodoWrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test("shoud data 'todo' equals props 'initialTodo'", () => {
+    expect(activeTodoWrapper.vm.todo).toEqual(todoData);
+  });
+
+  test("should computed data set by props", () => {
+    expect(activeTodoWrapper.vm.checkBtnType).toBe("");
+    expect(activeTodoWrapper.vm.todoStatus).toBe("");
+    expect(completedTodoWrapper.vm.checkBtnType).toBe("primary");
+    expect(completedTodoWrapper.vm.todoStatus).toBe("completed");
+  });
+
+  test("should render completed todo", () => {
+    expect(
+      completedTodoWrapper.find("input").classes("completed")
+    ).toBeTruthy();
+    expect(completedTodoWrapper.find("input").element.value).toBe(
+      todoData.content
+    );
+  });
+});
