1eac06e4df0f381918ae531660a7890cc9f273eb
Wang Lei <lei.wang1@thoughtworks.com>
2020-03-17 10:27:26 +0800

    feat: only show delete button when hover todo

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -46,23 +46,32 @@ export default {
 };
 </script>
 
-<style>
-.todo-wrapper {
-  margin: 20px 0px;
-  padding: 5px 0px;
-  border-bottom: solid 1px #dcdfe6;
-}
-.todo {
-  border: none;
-  outline: none;
-  font-size: 22px;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  color: #2c3e50;
-  width: 100%;
-  padding: 0px 15px;
-}
-.completed {
-  color: #bfc1c5;
-  text-decoration: line-through;
-}
+<style lang="sass" scoped>
+.todo-wrapper
+  margin: 20px 0px
+  padding: 5px 0px
+  border-bottom: solid 1px #dcdfe6
+
+  .delete-btn
+    color: #ffffff
+
+  .todo
+    border: none
+    outline: none
+    font-size: 22px
+    font-family: Avenir, Helvetica, Arial, sans-serif
+    color: #2c3e50
+    width: 90%
+    padding: 0px 15px
+
+  .completed
+    color: #bfc1c5
+    text-decoration: line-through
+
+.todo-wrapper:hover
+  .delete-btn
+    color: #606266
+
+  .delete-btn:hover
+    color: #409EFF
 </style>
